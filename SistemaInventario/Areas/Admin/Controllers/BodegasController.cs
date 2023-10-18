b5f321baa118b7a74b52226f01cd1259f9acd1cb
Hector Ivan <hirfleites@gmail.com>
2023-10-18 12:02:23 -0400

    Módulo 5 Case 6

---
+++ SistemaInventario/Areas/Admin/Controllers/BodegasController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaInventario.AccesosDatos.Repositorio.IRepositorio;
+using System.Drawing.Text;
+
+namespace SistemaInventario.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BodegasController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+        public BodegasController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region API
+        [HttpGet]
+
+        public IActionResult ObtenerTodos() 
+        {
+            var todos = _unidadTrabajo.Bodega.ObtenerTodos();
+            return Json(new {data = todos});
+        }
+
+        #endregion
+    }
+}
---
+++ SistemaInventario/wwwroot/js/bodega.js
@@ -0,0 +1,28 @@
+﻿var datatable 
+
+$(document).ready(function () {
+    loadDataTable()
+});
+
+function loadDaraTable() {
+    datatable = $('#tblDatos').DataTable({
+        "ajax": {
+            "url":"/Admin/Bodega/ObtenerTodos"
+        },
+        "columns": [
+            {"data": "nombre", "width": "20%" },
+            {"data": "descripcion", "width": "20%"},
+            {"data": "estado", "width": "20%"},
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="text-center">
+                            <a href="Admin/Bodegas/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer"><i class="fas fa-edit"></i></a>
+                            <a class="btn btn-danger text-white" style="cursor:pointer"><i class="fas fa-trash"></i></a>
+                        </div>`;
+                }, "width": "20%"
+            }
+        ]
+    })
+}
